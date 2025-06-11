5f387253372ea2749e437dc72032fc0c6548562e
Ankan Saha <ankansahadaluabari@gmail.com>
2025-06-11 11:41:16 +0530

    Fix: Change VERSION declaration to const for better clarity and consistency

--- src/Root/main.go
+++ src/Root/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	VERSION := "3.11.16-stable"
+	const VERSION = "3.11.16-stable";
 
 	// handle version flag without requiring sudo
 	if len(os.Args) > 1 && os.Args[1] == "--version" {
